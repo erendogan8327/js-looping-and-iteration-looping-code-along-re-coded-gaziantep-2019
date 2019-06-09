bb6b38f6e9952bbdf9cf43de3b4c66fb832384c3
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-06-09 16:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+ function findMatching (array, name) {
+  const newCollection = [];
+  for (const user of array) {
+    if (user.toUpperCase() === name.toUpperCase()) {
+      newCollection.push(user);
+    }
+  }
+ 
+  return newCollection;
+}
+
+function fuzzyMatch(array, name){
+  const newCollection = [];
+  for (const user of array) {
+    if (user.startsWith(name)) {
+      newCollection.push(user);
+    }
+  }
+ 
+  return newCollection;
+}
+
+function matchName(array, isim){
+  const newCollection = [];
+  for (const user of array) {
+    if (user.name === isim) {
+      newCollection.push(user);
+    }
+  }
+ 
+  return newCollection;
+  
+  
+  
+}
